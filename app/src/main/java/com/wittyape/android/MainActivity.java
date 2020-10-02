7644d8131c1e4f82fd0191ec21d7713fd6204d85
shashiofficial <shashiofficial10@gmail.com>
2020-10-02 13:32:26 +0530

    Added : Update user details (Name + class)

--- app/src/main/java/com/wittyape/android/MainActivity.java
+++ app/src/main/java/com/wittyape/android/MainActivity.java
@@ -2,6 +2,7 @@ package com.wittyape.android;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -11,22 +12,35 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "debug_shashi";
     NavigationView navigationView;
@@ -34,6 +48,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     DrawerLayout drawerLayout;
 
     ProgressBar progressBar;
+
+    TextInputLayout textInputLayoutUserName;
+    Spinner spinnerClass;
+    String spinnerClassData = "";
+
+    ProgressBar progressBarUpdate;
+    ProgressBar progressBarLogout;
 
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
@@ -61,7 +82,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getUserClass();
             }
         };
-        Log.d(TAG, "onCreate: " + userClass);
 
         toggle = new ActionBarDrawerToggle(this,
                 drawerLayout,
@@ -200,15 +220,147 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         switch (menuId) {
             case R.id.menu_item_profile:
-                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                editUserProfile();
                 break;
-            case R.id.menu_item_logout:
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-                finish();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void editUserProfile() {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View layoutView = layoutInflater.inflate(R.layout.profile_dialog_layout, null);
+
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this).setView(layoutView);
+        alertDialog.setCancelable(true);
+        final AlertDialog testDialog = alertDialog.create();
+        testDialog.show();
+
+        textInputLayoutUserName = layoutView.findViewById(R.id.text_input_layout_profile_main);
+        spinnerClass = layoutView.findViewById(R.id.spinner_class_selector_update);
+        Button buttonUpdate = layoutView.findViewById(R.id.button_update_name_profile);
+        Button buttonLogout = layoutView.findViewById(R.id.button_sign_out_main);
+        progressBarUpdate = layoutView.findViewById(R.id.progress_bar_profile_update);
+        progressBarLogout = layoutView.findViewById(R.id.progress_bar_profile_sign_out);
+
+        progressBarUpdate.setVisibility(View.GONE);
+        progressBarLogout.setVisibility(View.GONE);
+
+        getUserName();
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String newTask = textInputLayoutUserName.getEditText().getText().toString();
+
+                if (newTask.isEmpty()) {
+                    textInputLayoutUserName.setError("Cannot be empty");
+                    return;
+                } else {
+                    textInputLayoutUserName.setError(null);
+                }
+
+                if (spinnerClassData.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Choose your class", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBarUpdate.setVisibility(View.VISIBLE);
+
+                String userClassChanged = spinnerClassData;
+
+                saveNewData(newTask, userClassChanged);
+                testDialog.dismiss();  // to dismiss
+
+            }
+        });
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutUser();
+            }
+        });
+
+        spinnerClass.setOnItemSelectedListener(this);
+
+    }
+
+    private void saveNewData(String newUserName, String userClassChanged) {
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", newUserName);
+        data.put("class", userClassChanged);
+
+        CollectionReference collectionReference = firebaseFirestore.collection(COLLECTION_NAME);
+
+        collectionReference
+                .document(firebaseAuth.getCurrentUser().getUid())
+                .update(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        progressBarUpdate.setVisibility(View.GONE);
+                        LinearLayout linearLayout = findViewById(R.id.linear_layout_main_activity);
+                        Snackbar snackbar = Snackbar.make(linearLayout, "Updated", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+
+                        getUserClass();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressBarUpdate.setVisibility(View.GONE);
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void getUserName() {
+
+        CollectionReference collectionReference = firebaseFirestore.collection(COLLECTION_NAME);
+
+        collectionReference.document(firebaseAuth.getCurrentUser().getUid()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                        if (documentSnapshot.exists()) {
+
+                            String name = documentSnapshot.getString("name");
+                            textInputLayoutUserName.getEditText().setText(name);
+
+                            spinnerClassData = documentSnapshot.getString("class");
+
+                            if (spinnerClassData.equals("Class1")) {
+                                spinnerClass.setSelection(1);
+                            } else if (spinnerClassData.equals("Class2")) {
+                                spinnerClass.setSelection(2);
+                            }
+
+                        }
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void logoutUser() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
     }
 
     @Override
@@ -226,6 +378,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (authStateListener != null) {
             firebaseAuth.removeAuthStateListener(authStateListener);
         }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if (adapterView.getSelectedItem().toString().equals("Select")) {
+            spinnerClassData = "";
+            return;
+        }
+
+        spinnerClassData = adapterView.getSelectedItem().toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
